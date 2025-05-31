5bca1a350f4aedb5bff2745797049788463c4477
al3xar <aarenast15@gmail.com>
2025-05-31 21:09:55 +0200

    feat: parte 5

---
+++ lab3/parte5/compilar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Script para compilar echosrv.c con los requisitos específicos
+
+# Compilar con:
+# - 32 bits (-m32)
+# - NX habilitado (por defecto)
+# - ASLR deshabilitado
+
+gcc -g -o echosrv64 echosrv64.c
